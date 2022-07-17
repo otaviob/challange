672b04d4b980dcef353282dd55170f0f430d3325
Otavio Brito <otaviob22@gmail.com>
2022-07-17 18:34:46 -0300

    Initial Commit

---
+++ iOS-Application-AlterSolutions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Application-AlterSolutions
+//
+//  Created by Otavio Brito on 17/07/22.
+//  Copyright © 2022 Otavio Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-Application-AlterSolutionsTests/iOS_Application_AlterSolutionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Application_AlterSolutionsTests.swift
+//  iOS-Application-AlterSolutionsTests
+//
+//  Created by Otavio Brito on 17/07/22.
+//  Copyright © 2022 Otavio Brito. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Application_AlterSolutions
+
+class iOS_Application_AlterSolutionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
