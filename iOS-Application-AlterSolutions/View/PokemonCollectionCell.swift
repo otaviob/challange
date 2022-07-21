dea64997a7a2816618e99a837cf7de9d6f5ab66b
Otavio Brito <otaviob22@gmail.com>
2022-07-21 12:05:27 -0300

    service api

---
+++ iOS-Application-AlterSolutions/View/PokemonCollectionCell.swift
@@ -0,0 +1,93 @@
+//
+//  PokemonCollectionCell.swift
+//  iOS-Application-AlterSolutions
+//
+//  Created by Otavio Brito on 17/07/22.
+//  Copyright © 2022 Otavio Brito. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    var pokemon: Pokemon? {
+        didSet {
+            nameLabel.text = pokemon?.name
+            imageView.image = pokemon?.image
+            
+            
+        }
+    }
+    
+    let imageView: UIImageView = {
+           let image = UIImageView()
+           image.backgroundColor = .groupTableViewBackground 
+           image.contentMode = .scaleAspectFit
+           return image
+       }()
+    
+     lazy var nameContainerView: UIView = {
+         let view = UIView()
+         view.backgroundColor = .mainPink()
+         
+         view.addSubview(nameLabel)
+         nameLabel.center(inView: view)
+         
+         return view
+     }()
+     
+     let nameLabel: UILabel = {
+         let label = UILabel()
+         label.textColor = .white
+         label.font = UIFont.systemFont(ofSize: 16)
+         label.text = "Bulbasaur"
+         return label
+     }()
+    
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureViewComponents()
+        
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleLongPress() {
+        print("handle long press..")
+    }
+    
+    // MARK: - Helper Function
+    
+    func configureViewComponents() {
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        
+        addSubview(imageView)
+        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: self.frame.height - 32)
+        
+        addSubview(nameContainerView)
+        nameContainerView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 32)
+        
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        self.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    
+    
+    
+    
+    
+}
